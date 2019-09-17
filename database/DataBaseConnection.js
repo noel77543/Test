2618a6f7c25d6e7c1db5049d21e39278c396bd6b
noel_yulon_energe <noel.sung@yulon-energy.com.tw>
2019-09-17 18:33:36 +0800

    完成對Database指定table 新增項目 並完成對應post API開設

--- database/DataBaseConnection.js
+++ database/DataBaseConnection.js
@@ -15,14 +15,22 @@ var queryCreateDatabase = "CREATE DATABASE " + DB_MEMBER_DATA_BASE;
 var queryCreateTable = "CREATE TABLE " +
     TABLE_NORMAL_MEMBER + "(" + COLUMN_NAME + "," + COLUMN_ADDRESS + ")" + ","
     + TABLE_VIP_MEMBER + "(" + COLUMN_NAME + "," + COLUMN_ADDRESS + ")";
-//新增資料
-var insertData ="";
+
+//新增一般會員資料
+var insertDataNormalMember = "INSERT INTO" + TABLE_NORMAL_MEMBER;
+//新增高級會員資料
+var insertDataVIPMember = "INSERT INTO" + TABLE_VIP_MEMBER;
+
+//查詢一般會員資料
+var selectDataNormalMember = "SELECT * FROM" + TABLE_NORMAL_MEMBER;
+//查詢高級會員資料
+var selectDataVIPMember = "SELECT * FROM" + TABLE_VIP_MEMBER
+
 //刪除資料
 var deleteData = "";
 //修改資料
 var updateData = "";
-//查詢資料
-var searchData = "";
+
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -33,13 +41,13 @@ var connection = mysql.createConnection({
     database: DB_MEMBER_DATA_BASE
 });
 
-connection.connect(function (err) {
-
-    // if (err) throw err;
-    console.log("Connected!");
-    queryAction(queryCreateDatabase,"DataBase Created");
+connection.connect(function (error) {
+    //如果不存在則創建
+    if (!dbIsExists(DB_MEMBER_DATA_BASE)) {
+        queryAction(queryCreateDatabase, "DataBase Created")
+        queryAction(queryCreateTable, "Table Created")
+    }
 });
-
 
 //--------
 
@@ -48,6 +56,88 @@ connection.connect(function (err) {
  */
 function queryAction(querySql, logString) {
     connection.query(querySql, function (error, result) {
+        // if (error) {
+        //     console.log("queryAction exception =" + error)
+        //     return;
+        // }
         console.log(logString);
     });
+}
+
+
+//-------------
+
+/***
+ *  insert NormalMember table的資料
+ *  使此方法為外部可引用 like java's public
+ */
+module.exports.addNormalMember = function (name, address) {
+    if (dbIsExists(DB_MEMBER_DATA_BASE)) {
+        insertDataNormalMember = insertDataNormalMember + "(" + name + "," + address + ")VALUES('Learn how to insert a new row',true)"
+        queryAction(insertDataNormalMember, "NormalMemberData Inserted");
+    }
+
+}
+
+
+//----------
+
+/***
+ * where NormalMember table的所有資料
+ * 使此方法為外部可飲用 like java's public
+ */
+module.exports.getAllNormalMember = function () {
+    if (dbIsExists(DB_MEMBER_DATA_BASE)) {
+        return connection.query(selectDataNormalMember, function (error, result) {
+            console.log("AllNormalMemberData Selected");
+            console.log(result);
+            return result
+        });
+    }
+
+}
+
+
+//----------
+
+/***
+ *  insert VIPMember table的資料
+ *  使此方法為外部可引用 like java's public
+ */
+module.exports.addVIPMember = function (name, address) {
+    if (dbIsExists(DB_MEMBER_DATA_BASE)) {
+        insertDataVIPMember = insertDataVIPMember + "(" + name + "," + address + ")VALUES('Learn how to insert a new row',true)"
+        queryAction(insertDataVIPMember, "VIPMemberData Inserted");
+    }
+
+}
+
+//----------
+
+/***
+ * where VIPMember table的所有資料
+ * 使此方法為外部可飲用 like java's public
+ */
+module.exports.getAllVIPMember = function () {
+    if (dbIsExists(DB_MEMBER_DATA_BASE)) {
+        connection.query(selectDataVIPMember, function (error, result) {
+            console.log("AllVIPMemberData Selected");
+            console.log(result);
+        });
+    }
+
+}
+
+
+//----------
+
+/***
+ *  DB 是否存在
+ */
+function dbIsExists(dataBaseName) {
+    var XMLHttpRequest = require("XMLHttpRequest").XMLHttpRequest;
+    var client = new XMLHttpRequest();
+    client.open('HEAD', dataBaseName, false);
+    client.send();
+    return client.status != 404;
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 
 const _API_TEST = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var dtaBaseConnection = require('../database/DataBaseConnection');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
@@ -8,6 +9,7 @@ var client = new XMLHttpRequest();
 client.responseType = 'json';
 client.timeout = 20 * 1000;
 
+//---------
 
 /***
  * 首頁網址 
@@ -23,30 +25,84 @@ router.get('/', function (req, res, next) {
   });
 });
 
+//---------
 
 /***
  * API GET  介接網路資源
  * url : http://localhost:port/getSuperheroes
  */
-router.get('/getSuperheroes', function (req, res, next) {
-  console.log('連線狀態:' + res.statusCode);
+router.get('/getSuperheroes', function (request, response, next) {
+  console.log('連線狀態:' + response.statusCode);
   client.open('GET', _API_TEST);
   client.send();
 
   client.onload = function () {
     var data = client.responseText;
-    res.send(data)
+    response.send(data)
   }
 });
+
+//---------
 
 /***
  * API GET  load local json - member address
  * url : http://localhost:port/getMemberAddress
  */
-router.get('/getMemberAddress', function (req, res, next) {
-  console.log('連線狀態:' + res.statusCode);
-  res.send(JSON.parse(fs.readFileSync('files/TestJson.json')));
+router.get('/getMemberAddress', function (request, response, next) {
+  console.log('連線狀態:' + response.statusCode);
+  response.send(JSON.parse(fs.readFileSync('files/TestJson.json')));
 });
+
+//---------
+
+/***
+ * API POST  新增一般會員至DB
+ * requestBody: {
+ *   "name":"name1",
+ *   "address":"address1"
+ * }
+ */
+router.post('/addNormalMember', function (request, response, next) {
+  console.log('連線狀態:' + response.statusCode);
+  var requestBody = request.body;
+  dtaBaseConnection.addNormalMember(requestBody.name, requestBody.address);
+  response.send("已新增一般會員,名稱:" + requestBody.name + ",地址:" + requestBody.address);
+})
+
+//---------
+
+/***
+ * API GET 至DB取得所有一般會員
+ */
+router.get('/getAllNormalMember', function (request, response, next) {
+  console.log('連線狀態:' + response.statusCode);
+  response.send(JSON.stringify(dtaBaseConnection.getAllNormalMember()));
+})
+
+
+//---------
+
+
+/***
+ * API POST 新增高級會員至DB
+ * requestBody: {
+ *   "name":"name1",
+ *   "address":"address1"
+ * }
+ */
+router.post('/addVIPMember', function (request, response, next) {
+  console.log('連線狀態:' + response.statusCode);
+  var requestBody = request.body;
+  dtaBaseConnection.addVIPMember(requestBody.name, requestBody.address);
+  response.send("已新增VIP會員,名稱:" + requestBody.name + ",地址:" + requestBody.address);
+})
+
+
+//---------
+
+/***
+ * API GET 至DB取得所有高級會員
+ */
 
 
 module.exports = router;
